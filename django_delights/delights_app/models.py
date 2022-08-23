fe227a113fe49138f7e1cb2b51d6479f4aa8e87a
jbierc <j.bierc@icloud.com>
2022-08-23 18:39:59 +0200

    writing models

--- django_delights/delights_app/models.py
+++ django_delights/delights_app/models.py
@@ -1,3 +1,20 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    quantity = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.IntegerField(default=0)
+    ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+class RecipeRequirements(models.Model):
+    ingredients_list = models.ForeignKey(MenuItem.ingredients, on_delete=models.CASCADE)
+
+class Purchase(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateField(default=datetime.date.today)
