57601ac872d398657a32505b0319eb23909160c3
jbierc <j.bierc@icloud.com>
2022-08-26 21:06:51 +0200

    created superuser, registered models

--- django_delights/delights_app/admin.py
+++ django_delights/delights_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
+admin.site.register(RecipeRequirements)
+admin.site.register(Purchase)
+
