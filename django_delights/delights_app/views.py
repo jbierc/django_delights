6585ff1e04042ee310a54cb1eb07dee81862970e
jbierc <j.bierc@icloud.com>
2022-09-07 18:30:50 +0200

    added login to views

--- django_delights/delights_app/views.py
+++ django_delights/delights_app/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
-# Create your views here.
-def index(request):
+
+def login_view(request):
+  context = {"login_view": "active"}
+
+  if request.method == "POST":
+    username = request.POST["username"]
+    password = request.POST["password"]
+ 
+    user = authenticate(request, username=username, password=password)
+ 
+    if user is not None:
+      return redirect("home.html")
+    else:
+      return HttpResponse("Invalid credentials!")
+  
+  return render(request, "registration/login.html", context)
+
+
+def home(request):
     return HttpResponse("Hello!")
--- django_delights/django_delights/urls.py
+++ django_delights/django_delights/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('delights/', include('delights_app.urls'))
+    path('', include('delights_app.urls'))
 ]
