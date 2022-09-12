d1f33b03689c7ed9742bf09e241ce20a4de47629
jbierc <j.bierc@icloud.com>
2022-09-12 19:50:59 +0200

    editing, signup form, forms

--- django_delights/delights_app/urls.py
+++ django_delights/delights_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('account/', include("django.contrib.auth.urls")),
-    path('logout/', views.logout_request, name='logout')
+    path('account/', include("django.contrib.auth.urls"), name='login'), #login, logout, password_change, password_reset
+    path('logout/', views.logout_request, name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup')
 ]
+
