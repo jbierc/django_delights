5c12f49b241df9d730a876b525de1c5fb3a54ac5
jbierc <j.bierc@icloud.com>
2023-04-19 18:09:51 +0200

    writing simple test

--- django_delights/delights_app/tests.py
+++ django_delights/delights_app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Ingredient
 
 # Create your tests here.
+class IngredientTests(TestCase):
+    def test_ingredient_unit(self):
+        new_ingredient = Ingredient(name = 'egg', unit = 'tbsp')
+        new_ingredient.full_clean()
+        
