5d66bbb8ca73d86a61261a929559340af8fbfdaa
jbierc <j.bierc@icloud.com>
2023-07-20 15:26:44 +0200

    added test folder and sample code

--- django_delights/delights_app/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from .models import Ingredient
-
-# Create your tests here.
-class IngredientTests(TestCase):
-    def test_ingredient_unit(self):
-        new_ingredient = Ingredient(name = 'egg', unit = 'tbsp')
-        new_ingredient.full_clean()
-        
