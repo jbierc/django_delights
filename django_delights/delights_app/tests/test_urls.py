1cca28b0ad3c7f30a153126a7bf1539dd1c2cad6
jbierc <j.bierc@icloud.com>
2023-07-20 16:49:44 +0200

    urls testing

--- django_delights/delights_app/tests/test_urls.py
+++ django_delights/delights_app/tests/test_urls.py
@@ -1,5 +1,15 @@
 from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from delights_app.views import home, PasswordChange
+
 
 class TestUrls(SimpleTestCase):
     def test_home_url_is_resolved(self):
-       assert 1 == 2
+       url = reverse('home')
+       #print(resolve(url))
+       self.assertEquals(resolve(url).func, home)
+
+    def test_login_url_is_resolved(self):
+       url = reverse('passwordchange')
+       #print(resolve(url))
+       self.assertEquals(resolve(url).func.view_class, PasswordChange)
