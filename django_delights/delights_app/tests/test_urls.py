0d1a9618ba8eea621250c3c309f6729f9a21ab73
jbierc <j.bierc@icloud.com>
2023-08-30 15:56:40 +0200

    rewriting urls tests

--- django_delights/delights_app/tests/test_urls.py
+++ django_delights/delights_app/tests/test_urls.py
@@ -1,15 +1,30 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, Client
 from django.urls import reverse, resolve
-from delights_app.views import home, PasswordChange
+from delights_app.views import *
 
 
 class TestUrls(SimpleTestCase):
+    def setUp(self):
+       self.client = Client()
+
     def test_home_url_is_resolved(self):
        url = reverse('home')
-       #print(resolve(url))
        self.assertEquals(resolve(url).func, home)
+       self.assertTemplateUsed('home.html')
 
     def test_login_url_is_resolved(self):
+       url = reverse('login')
+       response = self.client.get(url)
+       self.assertEqual(response.status_code, 200)
+       self.assertTemplateUsed(response, 'registration/login.html')
+       
+    def test_logout_url_is_resolved(self):
+       url = reverse('logout')
+       response = self.client.get(url)
+       self.assertEqual(response.status_code, 302)
+
+    def test_passwordchange_url_is_resolved(self):
        url = reverse('passwordchange')
-       #print(resolve(url))
+       response = self.client.get(url)
        self.assertEquals(resolve(url).func.view_class, PasswordChange)
+       self.assertTemplateUsed(response, 'registration/password_change.html')
